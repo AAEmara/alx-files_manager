8e037a72778f03dec94acd040c8301c7682b55b6
AAEmara <abdelrahmanemara18@gmail.com>
2024-10-17 04:45:37 +0300

    FMNGR: Returning UsersController back.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -47,7 +47,7 @@ export default class UserController {
 
       return res.status(200).json({ id: _id, email });
     } catch (error) {
-      res.status(401).json({ error: 'Unauthorized' });
+      return res.status(401).json({ error: 'Unauthorized' });
     }
   }
 }
